858d1154d9039e1162cd001d4b379227f9fc8fb6
Anwesha Das <anwesharimi123@gmail.com>
2024-08-13 20:15:10 +0530

    first commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,7 +11,16 @@ function About() {
       <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p>
-        We're pizza alchemists, transforming simple ingredients into flavor masterpieces. From the secret family sauce simmered for hours to the hand-tossed dough blistered in our stone oven, every step is infused with passion. We don't just serve pizza; we create edible happiness, one slice at a time. More than just a pizzeria, we're your neighborhood haven. We've been fueling family dinners, birthday celebrations, and friendly gatherings for years. Whether you're cheering on your favorite team or catching up with friends, our warm atmosphere and delicious pizzas are the perfect recipe for making memories.
+          We're pizza alchemists, transforming simple ingredients into flavor
+          masterpieces. From the secret family sauce simmered for hours to the
+          hand-tossed dough blistered in our stone oven, every step is infused
+          with passion. We don't just serve pizza; we create edible happiness,
+          one slice at a time. More than just a pizzeria, we're your
+          neighborhood haven. We've been fueling family dinners, birthday
+          celebrations, and friendly gatherings for years. Whether you're
+          cheering on your favorite team or catching up with friends, our warm
+          atmosphere and delicious pizzas are the perfect recipe for making
+          memories.
         </p>
       </div>
     </div>
